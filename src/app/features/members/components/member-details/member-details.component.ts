8630c7b068b0f3e92133e1e1913c72175cec9b43
Sandeep Singh <sandeep.singh@catalisgov.com>
2026-02-14 13:10:12 -0700

    styles introduction

--- src/app/features/members/components/member-details/member-details.component.ts
+++ src/app/features/members/components/member-details/member-details.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Member } from '../../models/member.model';
+import { CommonModule, DatePipe } from '@angular/common';
+import { SharedModule } from '../../../../shared/shared.module';
 
 @Component({
   selector: 'app-member-details',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, CommonModule, SharedModule],
   templateUrl: './member-details.component.html',
   styleUrl: './member-details.component.scss'
 })
 export class MemberDetailsComponent {
   @Input() member?: Member;
+  isExpired(date: string): boolean {
+    return new Date(date) < new Date();
+  }
 
 }
